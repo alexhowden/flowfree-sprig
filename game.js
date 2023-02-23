6b6f3b5dcc7c12e3f38494a71ebf04635ffb76c7
Alex Howden <ahowden27@gmail.com>
2023-02-22 23:10:59 -0500

    Update game.js

--- game.js
+++ game.js
@@ -2,20 +2,13 @@
 @title: connect the dots
 @author: pxkka
 shit to do
-- make it so you can only drag starting on an endpoint
-- make it so you can drop off on an endpoint of same color, and if not, delete the chain
+- make sure only one line of each color
 - win logic
 - make a ton of levels for the 5 mins of gameplay
 */
 
 const player = "c";
 const playerToggle = "C";
-const red = "r";
-const orange = "o";
-const yellow = "y";
-const green = "g";
-const blue = "b";
-const purple = "p";
 
 const redEnd = "R";
 const orangeEnd = "O";
@@ -23,6 +16,13 @@ const yellowEnd = "Y";
 const greenEnd = "G";
 const blueEnd = "B";
 const purpleEnd = "P";
+
+const red = "r";
+const orange = "o";
+const yellow = "y";
+const green = "g";
+const blue = "b";
+const purple = "p";
 
 const dict = {"R": red, "O": orange, "Y": yellow, "G": green, "B": blue, "P": purple,
               "r": red, "o": orange, "y": yellow, "g": green, "b": blue, "p": purple};
@@ -62,108 +62,6 @@ setLegend(
 11............11
 1111111111111111
 1111111111111111`],
-  [red, bitmap`
-3333333333333333
-3333333333333333
-3333333333333333
-3333333333333333
-3333333333333333
-3333333333333333
-3333333333333333
-3333333333333333
-3333333333333333
-3333333333333333
-3333333333333333
-3333333333333333
-3333333333333333
-3333333333333333
-3333333333333333
-3333333333333333`],
-  [orange, bitmap`
-9999999999999999
-9999999999999999
-9999999999999999
-9999999999999999
-9999999999999999
-9999999999999999
-9999999999999999
-9999999999999999
-9999999999999999
-9999999999999999
-9999999999999999
-9999999999999999
-9999999999999999
-9999999999999999
-9999999999999999
-9999999999999999`],
-  [yellow, bitmap`
-6666666666666666
-6666666666666666
-6666666666666666
-6666666666666666
-6666666666666666
-6666666666666666
-6666666666666666
-6666666666666666
-6666666666666666
-6666666666666666
-6666666666666666
-6666666666666666
-6666666666666666
-6666666666666666
-6666666666666666
-6666666666666666`],
-  [green, bitmap`
-4444444444444444
-4444444444444444
-4444444444444444
-4444444444444444
-4444444444444444
-4444444444444444
-4444444444444444
-4444444444444444
-4444444444444444
-4444444444444444
-4444444444444444
-4444444444444444
-4444444444444444
-4444444444444444
-4444444444444444
-4444444444444444`],
-  [blue, bitmap`
-7777777777777777
-7777777777777777
-7777777777777777
-7777777777777777
-7777777777777777
-7777777777777777
-7777777777777777
-7777777777777777
-7777777777777777
-7777777777777777
-7777777777777777
-7777777777777777
-7777777777777777
-7777777777777777
-7777777777777777
-7777777777777777`],
-  [purple, bitmap`
-HHHHHHHHHHHHHHHH
-HHHHHHHHHHHHHHHH
-HHHHHHHHHHHHHHHH
-HHHHHHHHHHHHHHHH
-HHHHHHHHHHHHHHHH
-HHHHHHHHHHHHHHHH
-HHHHHHHHHHHHHHHH
-HHHHHHHHHHHHHHHH
-HHHHHHHHHHHHHHHH
-HHHHHHHHHHHHHHHH
-HHHHHHHHHHHHHHHH
-HHHHHHHHHHHHHHHH
-HHHHHHHHHHHHHHHH
-HHHHHHHHHHHHHHHH
-HHHHHHHHHHHHHHHH
-HHHHHHHHHHHHHHHH`],
   [redEnd, bitmap`
 0000000000000000
 0333333333333330
@@ -265,7 +163,109 @@ HHHHHHHHHHHHHHHH`],
 0HHHHHHHHHHHHHH0
 0HHHHHHHHHHHHHH0
 0HHHHHHHHHHHHHH0
-0000000000000000`]
+0000000000000000`],
+  [red, bitmap`
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333`],
+  [orange, bitmap`
+9999999999999999
+9999999999999999
+9999999999999999
+9999999999999999
+9999999999999999
+9999999999999999
+9999999999999999
+9999999999999999
+9999999999999999
+9999999999999999
+9999999999999999
+9999999999999999
+9999999999999999
+9999999999999999
+9999999999999999
+9999999999999999`],
+  [yellow, bitmap`
+6666666666666666
+6666666666666666
+6666666666666666
+6666666666666666
+6666666666666666
+6666666666666666
+6666666666666666
+6666666666666666
+6666666666666666
+6666666666666666
+6666666666666666
+6666666666666666
+6666666666666666
+6666666666666666
+6666666666666666
+6666666666666666`],
+  [green, bitmap`
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444`],
+  [blue, bitmap`
+7777777777777777
+7777777777777777
+7777777777777777
+7777777777777777
+7777777777777777
+7777777777777777
+7777777777777777
+7777777777777777
+7777777777777777
+7777777777777777
+7777777777777777
+7777777777777777
+7777777777777777
+7777777777777777
+7777777777777777
+7777777777777777`],
+  [purple, bitmap`
+HHHHHHHHHHHHHHHH
+HHHHHHHHHHHHHHHH
+HHHHHHHHHHHHHHHH
+HHHHHHHHHHHHHHHH
+HHHHHHHHHHHHHHHH
+HHHHHHHHHHHHHHHH
+HHHHHHHHHHHHHHHH
+HHHHHHHHHHHHHHHH
+HHHHHHHHHHHHHHHH
+HHHHHHHHHHHHHHHH
+HHHHHHHHHHHHHHHH
+HHHHHHHHHHHHHHHH
+HHHHHHHHHHHHHHHH
+HHHHHHHHHHHHHHHH
+HHHHHHHHHHHHHHHH
+HHHHHHHHHHHHHHHH`]
 );
 
 const levels = [
@@ -276,17 +276,18 @@ c....
 .....
 .....`,
   map`
-c....
+c..R.
 .....
 ..B..
 .....
-....B`,
+.R..B`,
 ];
 
 let level = 0;
 let toggle = 0;
-let xList = [];
-let yList = [];
+let startPos = [];
+let endPos = [];
+let dragSprt = "";
 
 addText("WASD: Movement", {x: 3, y: 5, color: color`0`});
 addText("I: Toggle Drag", {x: 3, y: 8, color: color`0`});
@@ -307,12 +308,10 @@ onInput("w", () => {
   } else {
     if (getTile(getFirst(playerToggle).x, getFirst(playerToggle).y - 1) == "" || ["R", "O", "Y", "G", "B", "P"].includes(getTile(getFirst(playerToggle).x, getFirst(playerToggle).y - 1)[0]["type"])) {
       const sprt = getTile(getFirst(playerToggle).x, getFirst(playerToggle).y)[1]["type"];
-      if (getTile(getFirst(playerToggle).x, getFirst(playerToggle).y - 1) == "" || ["R", "O", "Y", "G", "B", "P"].includes(getTile(getFirst(playerToggle).x, getFirst(playerToggle).y - 1)[0]["type"])) {
-        getFirst(playerToggle).y -= 1;
-        xList.push(getFirst(playerToggle).x);
-        yList.push(getFirst(playerToggle).y);
+      getFirst(playerToggle).y -= 1;
+      if (!["R", "O", "Y", "G", "B", "P"].includes(getTile(getFirst(playerToggle).x, getFirst(playerToggle).y)[getTile(getFirst(playerToggle).x, getFirst(playerToggle).y).length - 1]["type"])) {
         addSprite(getFirst(playerToggle).x, getFirst(playerToggle).y, dict[sprt]);
-      }
+      }    
     }
   }
 });
@@ -323,12 +322,10 @@ onInput("a", () => {
   } else {
     if (getTile(getFirst(playerToggle).x - 1, getFirst(playerToggle).y) == "" || ["R", "O", "Y", "G", "B", "P"].includes(getTile(getFirst(playerToggle).x - 1, getFirst(playerToggle).y)[0]["type"])) {
       const sprt = getTile(getFirst(playerToggle).x, getFirst(playerToggle).y)[1]["type"];
-      if (getTile(getFirst(playerToggle).x - 1, getFirst(playerToggle).y) == "" || ["R", "O", "Y", "G", "B", "P"].includes(getTile(getFirst(playerToggle).x - 1, getFirst(playerToggle).y)[0]["type"])) {
-        getFirst(playerToggle).x -= 1;
-        xList.push(getFirst(playerToggle).x);
-        yList.push(getFirst(playerToggle).y);
+      getFirst(playerToggle).x -= 1;
+      if (!["R", "O", "Y", "G", "B", "P"].includes(getTile(getFirst(playerToggle).x, getFirst(playerToggle).y)[getTile(getFirst(playerToggle).x, getFirst(playerToggle).y).length - 1]["type"])) {
         addSprite(getFirst(playerToggle).x, getFirst(playerToggle).y, dict[sprt]);
-      }
+      }    
     }
   }
 });
@@ -339,12 +336,10 @@ onInput("s", () => {
   } else {
     if (getTile(getFirst(playerToggle).x, getFirst(playerToggle).y + 1) == "" || ["R", "O", "Y", "G", "B", "P"].includes(getTile(getFirst(playerToggle).x, getFirst(playerToggle).y + 1)[0]["type"])) {
       const sprt = getTile(getFirst(playerToggle).x, getFirst(playerToggle).y)[1]["type"];
-      if (getTile(getFirst(playerToggle).x, getFirst(playerToggle).y + 1) == "" || ["R", "O", "Y", "G", "B", "P"].includes(getTile(getFirst(playerToggle).x, getFirst(playerToggle).y + 1)[0]["type"])) {
-        getFirst(playerToggle).y += 1;
-        xList.push(getFirst(playerToggle).x);
-        yList.push(getFirst(playerToggle).y);
+      getFirst(playerToggle).y += 1;
+      if (!["R", "O", "Y", "G", "B", "P"].includes(getTile(getFirst(playerToggle).x, getFirst(playerToggle).y)[getTile(getFirst(playerToggle).x, getFirst(playerToggle).y).length - 1]["type"])) {
         addSprite(getFirst(playerToggle).x, getFirst(playerToggle).y, dict[sprt]);
-      }
+      }    
     }
   }
 });
@@ -355,19 +350,16 @@ onInput("d", () => {
   } else {
     if (getTile(getFirst(playerToggle).x + 1, getFirst(playerToggle).y) == "" || ["R", "O", "Y", "G", "B", "P"].includes(getTile(getFirst(playerToggle).x + 1, getFirst(playerToggle).y)[0]["type"])) {
       const sprt = getTile(getFirst(playerToggle).x, getFirst(playerToggle).y)[1]["type"];
-      if (getTile(getFirst(playerToggle).x + 1, getFirst(playerToggle).y) == "" || ["R", "O", "Y", "G", "B", "P"].includes(getTile(getFirst(playerToggle).x + 1, getFirst(playerToggle).y)[0]["type"])) {
-        getFirst(playerToggle).x += 1;
-        xList.push(getFirst(playerToggle).x);
-        yList.push(getFirst(playerToggle).y);
+      getFirst(playerToggle).x += 1;
+      if (!["R", "O", "Y", "G", "B", "P"].includes(getTile(getFirst(playerToggle).x, getFirst(playerToggle).y)[getTile(getFirst(playerToggle).x, getFirst(playerToggle).y).length - 1]["type"])) {
         addSprite(getFirst(playerToggle).x, getFirst(playerToggle).y, dict[sprt]);
       }
-      
     }
   }
 });
 
 onInput("i", () => {
-  toggleDrag();
+    toggleDrag();
 });
 
 onInput("k", () => {
@@ -375,10 +367,7 @@ onInput("k", () => {
 });
 
 afterInput(() => {
-  if (level == 0) {
-    level++;
-    changeLevel();
-  } else if (tilesWith(player, red, orange, yellow, green, blue, purple).length == currentLevel.length) {
+  if (level == 0 || tilesWith(player, red, orange, yellow, green, blue, purple).length == currentLevel.length) {
     level++;
     changeLevel();
   }
@@ -398,25 +387,23 @@ function changeLevel() {
 
 function toggleDrag() {
   if (toggle == 0) {
-    if (getTile(getFirst(player).x, getFirst(player).y).length > 1) {
-      if (["R", "O", "Y", "G", "B", "P"].includes(getTile(getFirst(player).x, getFirst(player).y)[1]["type"])) {
-        toggle = 1;
-        getFirst(player).type = playerToggle;
-      }
+    dragSprt = getTile(getFirst(player).x, getFirst(player).y)[1]["type"];
+    if (getTile(getFirst(player).x, getFirst(player).y).length > 1 && ["R", "O", "Y", "G", "B", "P"].includes(getTile(getFirst(player).x, getFirst(player).y)[1]["type"])) {
+      toggle = 1;
+      getFirst(player).type = playerToggle;
+      startPos = [getFirst(playerToggle).x, getFirst(playerToggle).y];
+      console.log(startPos);
     }
   } else {
-    const sprt = getTile(getFirst(playerToggle).x, getFirst(playerToggle).y)[1]["type"];
-    if (getTile(getFirst(playerToggle).x, getFirst(playerToggle).y).length != 1) {
-      toggle = 0;
-      getFirst(playerToggle).type = player;
-    }
-    console.log(getTile(getFirst(player).x, getFirst(player).y).length);
-    if (["r", "o", "y", "g", "b", "p"].includes(sprt) && getTile(getFirst(player).x, getFirst(player).y).length < 4) {
-      for (var i = 0; i < xList.length - 1; i++) {
-        clearTile(xList[i], yList[i]);
+    toggle = 0;
+    getFirst(playerToggle).type = player;
+    endPos = [getFirst(player).x, getFirst(player).y];
+    console.log(getTile(startPos[0], startPos[1])[0]["type"]);
+    console.log(getTile(endPos[0], endPos[1])[getTile(endPos[0], endPos[1]).length - 1]["type"]);
+    if (startPos == endPos || getTile(startPos[0], startPos[1])[0]["type"] != getTile(endPos[0], endPos[1])[getTile(endPos[0], endPos[1]).length - 1]["type"] || (getTile(endPos[0], endPos[1]).length >= 3 && !["R", "O", "Y", "G", "B", "P"].includes(dragSprt))) {
+      while (getAll(dict[dragSprt]).length > 0) {
+        getFirst(dict[dragSprt]).remove();
       }
-      xList = [];
-      yList = [];
     }
   }
 }
